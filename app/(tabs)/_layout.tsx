c7bfe85b0efb27dc9f51a39ecaf447d65e299bcf
logan-gameing <Logan.Jones1@edu.sait.ca>
2026-02-21 21:43:53 -0700

    Combined reels branch (main) with master

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,93 @@
+import { Image } from "expo-image";
 import { Tabs } from "expo-router";
+import { StyleSheet, View } from "react-native";
 
 export default function Layout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="reels" options={{ title: "Reels" }} />
+    <Tabs screenOptions={{ headerShown: false, tabBarBackground: Footer }}>
+      <Tabs.Screen
+        name="main-page"
+        options={{
+          title: "",
+          tabBarIcon: () => (
+            <Image
+              source={require("@/assets/instagram/icons/home.png")}
+              style={styles.icons}
+            ></Image>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search-page"
+        options={{
+          title: "",
+          tabBarIcon: () => (
+            <Image
+              source={require("@/assets/instagram/icons/search.png")}
+              style={styles.icons}
+            ></Image>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="more"
+        listeners={{
+          tabPress: (e) => {
+            e.preventDefault();
+          },
+        }}
+        options={{
+          title: "",
+          tabBarIcon: () => (
+            <Image
+              source={require("@/assets/instagram/icons/more.png")}
+              style={styles.icons}
+            ></Image>
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="reels"
+        options={{
+          title: "",
+          tabBarIcon: () => (
+            <Image
+              source={require("@/assets/instagram/icons/video.png")}
+              style={styles.icons}
+            ></Image>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile-page"
+        options={{
+          title: "",
+          tabBarIcon: () => (
+            <Image
+              source={require("@/assets/instagram/icons/search.png")}
+              style={styles.icons}
+            ></Image>
+          ),
+        }}
+      />
     </Tabs>
   );
 }
+
+const Footer = () => {
+  return <View style={styles.footerbox}></View>;
+};
+
+const styles = StyleSheet.create({
+  footerbox: {
+    height: 60,
+    width: "100%",
+    backgroundColor: "#0c1014",
+  },
+  icons: {
+    height: 30,
+    width: 30,
+    filter: "invert(1)",
+  },
+});
