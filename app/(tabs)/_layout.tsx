274384e2f5cc7cfa5d55b25963c3c2a3d30711ac
logan-gameing <Logan.Jones1@edu.sait.ca>
2026-02-21 19:35:02 -0700

    Working on layout still trying footer background change

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,7 +4,7 @@ import { StyleSheet } from "react-native";
 
 export default function Layout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
+    <Tabs screenOptions={{ headerShown: false, tabBarBackground: "#000000" }}>
       <Tabs.Screen
         name="main-page"
         options={{
@@ -29,7 +29,62 @@ export default function Layout() {
           ),
         }}
       />
+      <Image
+        source={require("@/assets/instagram/icons/more.png")}
+        style={styles.icons}
+      ></Image>
+      <Tabs.Screen
+        name="reels-page"
+        options={{
+          title: "",
+          tabBarIcon: () => (
+            <Image
+              source={require("@/assets/instagram/icons/video.png")}
+              style={styles.icons}
+            ></Image>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile-page"
+        options={{
+          title: "",
+          tabBarIcon: () => (
+            <Image
+              source={require("@/assets/instagram/icons/search.png")}
+              style={styles.icons}
+            ></Image>
+          ),
+        }}
+      />
     </Tabs>
+  );
+}
+
+const Footer() {
+  return (
+    <View style={styles.footerbox}>
+      <Image
+        source={require("../assets/instagram/icons/home.png")}
+        style={styles.icons}
+      ></Image>
+      <Image
+        source={require("../assets/instagram/icons/search.png")}
+        style={styles.icons}
+      ></Image>
+      <Image
+        source={require("../assets/instagram/icons/more.png")}
+        style={styles.icons}
+      ></Image>
+      <Image
+        source={require("../assets/instagram/icons/video.png")}
+        style={styles.icons}
+      ></Image>
+      <Image
+        source={require("../assets/instagram/icons/search.png")}
+        style={styles.icons}
+      ></Image>
+    </View>
   );
 }
 
