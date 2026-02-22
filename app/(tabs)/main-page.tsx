235d2eb44a2aa6c82ffb9346d254f566796a0679
logan-gameing <Logan.Jones1@edu.sait.ca>
2026-02-21 20:27:47 -0700

    Fixed not being sent to the main page upon loading the program

--- app/(tabs)/main-page.tsx
+++ app/(tabs)/main-page.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Main() {
   return (
@@ -8,4 +8,4 @@ export default function Main() {
   );
 }
 
-const styles = StyleSheet.create({});
+
--- app/(tabs)/more.tsx
+++ app/(tabs)/more.tsx
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View } from "react-native";
 
-export default function Main() {
+export default function More() {
   return (
     <View>
       <Text>
