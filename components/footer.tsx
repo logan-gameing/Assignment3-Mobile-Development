804a3f972c15c9e219db6feaba00829883692833
logan-gameing <Logan.Jones1@edu.sait.ca>
2026-02-21 17:59:28 -0700

    created components folder and footer folder

---
+++ components/footer.tsx
@@ -0,0 +1,14 @@
+import { Image } from "expo-image";
+import { View } from "react-native";
+
+export default function Footer() {
+  return (
+    <View>
+      <Image></Image>
+      <Image></Image>
+      <Image></Image>
+      <Image></Image>
+      <Image></Image>
+    </View>
+  );
+}
