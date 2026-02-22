aca7d3a08c753a30eb09e8296b7bcf679996d364
logan-gameing <Logan.Jones1@edu.sait.ca>
2026-02-21 20:44:46 -0700

    Working on Main-Page

--- app/(tabs)/main-page.tsx
+++ app/(tabs)/main-page.tsx
@@ -1,14 +1,23 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, View } from "react-native";
 
 export default function Main() {
   return (
     <View style={styles.background}>
       <View style={styles.header}>
         <Image
-          source={require("@/assets/instagram/images/instagram-logo-words.png")}
+          style={styles.headerimage}
+          source={require("@/assets/instagram/images/instagram.png")}
         ></Image>
-        <Text>HELO</Text>
+        <Image
+          style={styles.headericons}
+          source={require("@/assets/instagram/icons/heart.png")}
+        ></Image>
+        <Image
+          style={styles.headericons}
+          source={require("@/assets/instagram/icons/comment.png")}
+        ></Image>
       </View>
+      <View></View>
     </View>
   );
 }
@@ -16,8 +25,25 @@ export default function Main() {
 const styles = StyleSheet.create({
   background: {
     backgroundColor: "#0c1014",
+    height: "100%",
   },
-  header: {},
+  header: {
+    padding: 5,
+    height: 65,
+    width: "100%",
+    flex: 1,
+    backgroundColor: "#000000",
+  },
+  headerimage: {
+    maxHeight: 70,
+    maxWidth: 140,
+    filter: "invert(0.5) brightness(2)",
+  },
+  headericons: {
+    height: 30,
+    width: 30,
+    filter: "invert(0.5) brightness(2)",
+  },
   peoplebox: {},
   peopleicons: {},
   topstory: {},
