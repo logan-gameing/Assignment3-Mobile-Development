9d629c556d8ec798b20dce8d58cbd084be025db7
logan-gameing <Logan.Jones1@edu.sait.ca>
2026-02-21 18:07:37 -0700

    Fully created templates to work off of

---
+++ components/main-page.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Main() {
+  return (
+    <View>
+      <Text> This page is to be worked on</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ components/reels-page.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Reels() {
+  return (
+    <View>
+      <Text> This page is to be worked on</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
